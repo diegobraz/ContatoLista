720cddccd3e7b3f0a2d5395d3923ac8a2ae1cf87
Diego_braz <you@example.com>
2021-03-19 20:53:25 -0300

    criando um menu

--- app/src/main/java/com/example/listacontato/MainActivity.kt
+++ app/src/main/java/com/example/listacontato/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.listacontato
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.listacontato.Adapter.Contato.ContatoAdapter
 import com.example.listacontato.Data.Contato
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
     private val rvList : RecyclerView by lazy {
@@ -40,5 +46,33 @@ class MainActivity : AppCompatActivity() {
 
             )
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    private fun toast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.item_menu1 ->{
+                toast("menu 1")
+                return true
+            }
+            R.id.item_menu2 ->{
+                toast("menu 2")
+                return true
+            }
+            R.id.item_menu3 ->{
+                toast("menu 3")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
